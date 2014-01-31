2396978b2e100407ce356b076526e43723596868
uupaa <uupaa.js@gmail.com>
2014-01-31 22:46:47 +0900

    add Test.swap(), Test.undo()

--- test/test.js
+++ test/test.js
@@ -2,9 +2,12 @@ new Test().add([
         testOK,
 //      testNG,
     ]).run().worker(function(err, test) {
-        if (!err && typeof Test_ !== "undefined") {
-            Test = Test_;
-            new Test(test).run().worker();
+        if (!err) {
+//          var undo = Test.swap(Test, Test_);
+
+            new Test(test).run().worker(function(err, test) {
+//              undo = Test.undo(undo);
+            });
         }
     });
 
